c2c32d90af986672a82cd6050198c0610b27d76a
Daniel Hall <daniel@danielhall.me>
2013-01-11 23:59:06 +1100

    Support questions expiring

--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,15 @@
+from datetime import date, timedelta
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from django.db.models import Q
 from quiz.models import Question
 
 def doquiz(request):
-    question = Question.objects.order_by('?')[0]
+    lastmonth = date.today() - timedelta(days=30)
+    try:
+        question = Question.objects.filter(Q(used__lte=lastmonth) | Q(used__isnull=True)).order_by('?')[0]
+    except IndexError as e:
+        return render_to_response('quiz_noquestions.html', {})
     return render_to_response('quiz_doquiz.html', {'question': question})
 
 def newquestion(request):
@@ -11,7 +17,7 @@ def newquestion(request):
 
 def answer(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    question.used = date.today()
+    question.save()
     return render_to_response('quiz_answer.html', {'question': question})
-
-
 
