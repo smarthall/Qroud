339f1b27a20256cbe4aff82f3ca142db6076f60c
Daniel Hall <daniel@danielhall.me>
2013-01-11 22:38:28 +1100

    Initial functionality

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^doquiz$', 'quiz.views.doquiz'),
     url(r'^newquestion$', 'quiz.views.newquestion'),
-    url(r'^question/(?P<question_id>\d+)/$', 'quiz.views.detail'),
+    url(r'^answer/(?P<question_id>\d+)/$', 'quiz.views.answer'),
 )
--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from quiz.models import Question
 
 def doquiz(request):
-    return HttpResponse("Hello, world. You want to do a quiz")
+    question = Question.objects.order_by('?')[0]
+    return render_to_response('quiz_doquiz.html', {'question': question})
 
 def newquestion(request):
     return HttpResponse("Hello, world. You want to enter a question")
 
-def detail(request, question_id):
-    return HttpResponse("Hello, world. You want to do question %s" % question_id)
+def answer(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render_to_response('quiz_answer.html', {'question': question})
 
 
 
