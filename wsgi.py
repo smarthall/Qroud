036831ef3007bf29da271fdff7f984f884ccd9f0
Daniel Hall <daniel@danielhall.me>
2013-01-12 15:27:27 +1100

    WORK DAMMIT!

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Qroud.settings")
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
