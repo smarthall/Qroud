df7da8c14f10795396915a63643bf7bee1ae7ffc
Daniel Hall <daniel@danielhall.me>
2013-01-12 20:13:57 +1100

    Simplified the question view

--- quiz/views.py
+++ quiz/views.py
@@ -14,7 +14,8 @@ def doquiz(request):
         question = Question.objects.filter(Q(used__lte=lastmonth) | Q(used__isnull=True)).order_by('?')[0]
     except IndexError as e:
         return render_to_response('quiz_noquestions.html', {})
-    return render_to_response('quiz_doquiz.html', {'question': question})
+    return render_to_response('quiz_details.html',
+                             {'question': question, 'showanswer': False})
 
 def newquestion(request):
     if request.method == 'POST':
@@ -31,5 +32,6 @@ def answer(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.used = date.today()
     question.save()
-    return render_to_response('quiz_answer.html', {'question': question})
+    return render_to_response('quiz_details.html',
+                             {'question': question, 'showanswer': True})
 
