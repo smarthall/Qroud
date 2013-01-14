eeaa222f3cf4dccb89ce3a31ef784d771175e478
Daniel Hall <daniel@danielhall.me>
2013-01-14 18:48:39 +1100

    Add slashes on the end of URLs

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^doquiz$', 'quiz.views.doquiz'),
-    url(r'^newquestion$', 'quiz.views.newquestion'),
+    url(r'^doquiz/$', 'quiz.views.doquiz'),
+    url(r'^newquestion/$', 'quiz.views.newquestion'),
     url(r'^answer/(?P<question_id>\d+)/$', 'quiz.views.answer'),
 )
 
